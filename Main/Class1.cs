9587c945e069d37689188a4d4ff93ccd1f8f1d38
Alranbs <116610409+Alranbs@users.noreply.github.com>
2022-10-24 22:13:36 -0300

    Add files via upload

---
+++ Main/Class1.cs
@@ -0,0 +1,273 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: alanv
+ * Date: 09/10/2022
+ * Time: 08:14 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections;
+
+namespace Main
+{
+	/// <summary>
+	/// Description of Class1.
+	/// </summary>
+	public class Medico
+	{
+		private string nombre,especialidad;
+		private int dni;
+		private ArrayList horario = new ArrayList () {"8:00","8:30","09:00","09:30","10:00","10:30","11:00","11:30","12:00"};
+		private ArrayList listapacientes;
+		private ArrayList listaturnos;
+		
+		public string NOMBRE
+		{
+			set{nombre=value;}
+			get{return nombre;}
+		}
+		
+		public string ESPECIALIDAD
+		{
+			set{especialidad=value;}
+			get{return especialidad;}
+		}
+		
+		public int DNI
+		{
+			set{dni=value;}
+			get{return dni;}
+		}
+		
+		public ArrayList LISTATURNOS
+		{
+			set{listaturnos = value;}
+			get{return listaturnos;}
+		}
+		public ArrayList LISTAPACIENTES
+		{
+			set{listapacientes = value;}
+			get{return listapacientes;}
+		}
+		
+		public ArrayList HORARIO
+		{
+			set{horario = value;}
+			get{return horario;}
+		}
+		
+		public Medico(string nombre, string especialidad, int dni, ArrayList listapacientes, ArrayList listaturnos)
+		{
+			this.nombre = nombre;
+			this.especialidad = especialidad;
+			this.dni = dni;
+			this.listapacientes = listapacientes;
+			this.listaturnos = listaturnos;
+		}
+		
+// Intenta agregar el turno, en caso de que no haya horario disponible aparece una excepcion
+
+		public void AgregarTurno(ArrayList listaturnos, ArrayList listapacientes, ArrayList horarios, string nombrep, string hora, int dni)
+		{
+			
+			try{
+				if (!horarios.Contains(hora))
+				{
+					throw new FueraDeHorario();
+				}
+				foreach(Turno elem in listaturnos)
+				{
+					if(elem.HORA == hora)
+					{
+						throw new SinHorarios();
+					}
+				}
+				Turno nueTurno = new Turno(nombrep, hora);
+				listaturnos.Add(nueTurno);
+				foreach(Paciente elem in listapacientes)
+				{
+					if(elem.DNI == dni)
+					{
+						elem.agregarTurno(nueTurno);
+					}
+				}
+				Console.WriteLine("Se ha agregado el turno");
+				Console.WriteLine("---------------");
+			}
+			catch(SinHorarios)
+			{
+				Console.WriteLine("Horario no disponible, llame el proximo dia de atencion");
+			}
+			catch(FueraDeHorario)
+			{
+				Console.WriteLine("Fuera de horario de atencion.");
+			}
+							
+		}
+		
+// Actualiza el diagnostico de un paciente a traves de su dni	
+
+		public void ActualizarDiag(ArrayList listapacientes)
+		{
+			Console.WriteLine("Ingrese el dni del paciente");
+			int dni = int.Parse(Console.ReadLine());
+			foreach (Paciente elem in listapacientes)
+			{
+				if (elem.DNI == dni)
+				{
+					Console.WriteLine("Ingrese el nuevo diagnostico del paciente");
+					elem.DIAG = Console.ReadLine();
+				}
+			}
+			Console.WriteLine("Se ha actualizado el diagnostico");
+			Console.WriteLine("---------------");			
+		}
+		
+// Cancela un turno a traves del horario del turno
+
+		public void CancTurno(ArrayList listaturnos)
+		{
+			int contador = 0;
+			Console.WriteLine("Ingrese el horario del turno a cancelar");
+			string horario = Console.ReadLine();
+			foreach (Turno elem in listaturnos)
+			{
+				if (horario == elem.HORA)
+				{
+					break;
+				}
+				contador = contador+1;
+			}
+			listaturnos.RemoveAt(contador);
+			Console.WriteLine("Se ha eliminado el turno");
+			Console.WriteLine("---------------");			
+		}
+		
+// Agrega un paciente a la lista de pacientes
+		
+		public void AgregarPaciente(ArrayList listapacientes)
+		{
+			Console.WriteLine("Ingrese el nombre del paciente");
+			string nombre = Console.ReadLine();
+			Console.WriteLine("Ingrese la obra social del paciente, en caso de no tener obra ingrese 'particular'");
+			string obra = Console.ReadLine();
+			Console.WriteLine("Ingrese el diagnostico del paciente");
+			string diag = Console.ReadLine();
+			Console.WriteLine("Ingrese el dni del paciente");
+			int dni = int.Parse(Console.ReadLine());
+			Console.WriteLine("Ingrese el numero de afiliado, en caso de no tener ingrese '0'");
+			int numob = int.Parse(Console.ReadLine());
+			Paciente paciente = new Paciente(nombre, obra, diag, dni, numob);
+			listapacientes.Add(paciente);
+			Console.WriteLine("Se agrego al paciente: " + nombre);
+			Console.WriteLine("---------------");			
+		}
+		
+// Elimina un paciente a traves de su dni		
+		
+		public void EliminarPaciente(ArrayList listaturnos, ArrayList listapacientes)
+		{
+			int contador = 0;
+			Console.WriteLine("Ingrese el dni del paciente a eliminar");
+			int dni = int.Parse(Console.ReadLine());
+			foreach(Paciente elem in listapacientes)
+			{
+				if (dni == elem.DNI)
+				{
+					foreach(Turno el in listaturnos)
+					{
+						if (elem.TURNO == el)
+						{
+							listaturnos.Remove(el);
+							break;
+						}
+					}
+					break;
+				}
+				contador = contador + 1;
+			}
+			listapacientes.RemoveAt(contador);
+			Console.WriteLine("Se ha eliminado al paciente");
+			Console.WriteLine("---------------");			
+		}
+		
+	}
+	public class Paciente
+	{
+		private string nombre,obra,diag;
+		private int dni,numob;
+		private Turno turno;
+		
+		public Paciente(string nombre, string obra, string diag, int dni, int numob)
+		{
+			this.nombre = nombre;
+			this.obra = obra;
+			this.diag = diag;
+			this.dni = dni;
+			this.numob = numob;
+			
+		}
+		public string NOMBRE
+		{
+			set {nombre=value;}
+			get {return nombre;}
+		}
+		public string OBRA
+		{
+			set{obra=value;}
+			get{return obra;}
+		}
+		public string DIAG
+		{
+			set{diag=value;}
+			get{return diag;}
+		}
+		public int DNI
+		{
+			set{dni=value;}
+			get{return dni;}
+		}
+		public int NUMOB
+		{
+			set{numob=value;}
+			get{return numob;}
+		}
+		public void agregarTurno(Turno turno){
+			this.turno = turno;
+		}
+		
+		public Turno TURNO
+		{
+			set{turno = value;}
+			get{return turno;}
+		}
+	}
+	
+	public class Turno
+	{
+		private string nombre,hora;
+		
+		public Turno(string nombre, string hora)
+		{
+			this.nombre = nombre;
+			this.hora = hora;
+		}
+		public string NOMBRE
+		{
+			set{nombre=value;}
+			get{return nombre;}
+		}
+		public string HORA
+		{
+			set{hora=value;}
+			get{return hora;}
+		}
+	}
+	public class SinHorarios: Exception
+	{	
+	}
+	public class FueraDeHorario : Exception
+	{		
+	}
+}
---
+++ Main/Program.cs
@@ -0,0 +1,153 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: alanv
+ * Date: 09/10/2022
+ * Time: 08:12 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections;
+
+namespace Main
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Paciente paciente1 = new Paciente("Jose", "Particular", "Dolor de cuello", 44709023, 0);
+			Paciente paciente2 = new Paciente("Manuel", "Particular", "Dolor de brazo", 22004433, 0);
+			Turno turno1 = new Turno("Jose", "08:30");
+			Turno turno2 = new Turno("Manuel", "09:00");
+			ArrayList listaturnos = new ArrayList();
+			ArrayList listapacientes = new ArrayList();
+			//listaturn.Add(turno1);
+			//listaturn.Add(turno2);
+			//listapac.Add(paciente1);
+			//listapac.Add(paciente2);
+			//paciente1.agregarTurno(turno1);
+			//paciente2.agregarTurno(turno2);	
+			Medico Jose = new Medico("Jose", "Traumatologia", 35523938, listapacientes, listaturnos);
+			bool t = true;
+			
+			do{
+				Console.WriteLine("Bienvenido al menu");
+				Console.WriteLine("---------------");
+				Console.WriteLine("Ingrese el numero correspondiente a la funcion que desea realizar");
+				Console.WriteLine("1) Dar un turno");
+				Console.WriteLine("2) Actualizar el diagnostico de un paciente");
+				Console.WriteLine("3) Cancelar un turno");
+				Console.WriteLine("4) Listado de los turnos");
+				Console.WriteLine("5) Lista de las obras sociales de los pacientes");
+				Console.WriteLine("6) Agregar paciente");
+				Console.WriteLine("7) Eliminar paciente");
+				Console.WriteLine("8) Lista de los pacientes");
+				Console.WriteLine("9) Salir del programa");
+				Console.WriteLine("---------------");
+				int eleccion = int.Parse(Console.ReadLine());
+			
+				switch(eleccion)
+				{
+					case 1:
+						DarTurno(Jose);
+						break;
+					case 2:
+						Jose.ActualizarDiag(Jose.LISTAPACIENTES);
+						break;
+					case 3:
+						Jose.CancTurno(Jose.LISTATURNOS);
+						break;
+					case 4:
+						ListaTurnos(Jose.LISTATURNOS);
+						break;
+					case 5:
+						ListaObras(Jose.LISTAPACIENTES);
+						break;
+					case 6:
+						Jose.AgregarPaciente(Jose.LISTAPACIENTES);
+						break;
+					case 7:
+						Jose.EliminarPaciente(Jose.LISTATURNOS, Jose.LISTAPACIENTES);
+						break;
+					case 8:
+						ListaPacientes(Jose.LISTAPACIENTES);
+						break;
+					case 9:
+						t = false;
+						break;
+					default :
+						Console.WriteLine("No existe esa opcion");
+						break;
+						
+				}
+			}
+			while (t);
+		}
+		
+// Se pasan los datos necesarios para dar el turno y se llama a la funcion AgregarTurno de la clase medico
+		
+		static public void DarTurno(Medico medico)
+		{
+			Console.WriteLine("---------------");
+			Console.WriteLine("Ingrese el nombre del paciente");
+			string nombre = Console.ReadLine();
+			Console.WriteLine("Ingrese el dni del paciente");
+			int dni = int.Parse(Console.ReadLine());
+			Console.WriteLine("Ingrese el horario en el siguiente formato: hh:mm");
+			string hora = Console.ReadLine();			
+			medico.AgregarTurno(medico.LISTATURNOS, medico.LISTAPACIENTES,medico.HORARIO, nombre, hora, dni);
+			
+		}
+		
+// Recorre la lista de turnos imprimiendo nombre y hora		
+
+		static public void ListaTurnos(ArrayList listaturnos)
+		{
+			Console.WriteLine("---------------");
+			foreach(Turno elem in listaturnos)
+			{
+				Console.WriteLine("Nombre:" + elem.NOMBRE);
+				Console.WriteLine("Horario:" + elem.HORA);	
+			}
+			Console.WriteLine("---------------");
+		}
+		
+// Crea una lista y luego agrega las obras sociales de la lista obras sociales sin repetirlas para luego imprimirlas		
+		
+		static public void ListaObras(ArrayList listapacientes)
+		{
+			ArrayList lista = new ArrayList();
+			foreach(Paciente elem in listapacientes)
+			{
+				if (!lista.Contains(elem.OBRA)){
+					lista.Add(elem.OBRA);
+				}
+			}
+			Console.WriteLine("Las obras sociales de los pacientes son las siguientes:");
+			Console.WriteLine("---------------");
+			foreach(string elem in lista)
+			{
+				Console.WriteLine(elem);
+			}
+			Console.WriteLine("---------------");
+		}
+
+// Recorre la lista de pacientes e imprime los datos de los mismos
+		
+		static public void ListaPacientes(ArrayList listapacientes)
+		{
+			foreach (Paciente elem in listapacientes)
+			{
+				Console.WriteLine("---------------");
+				Console.WriteLine("Nombre: " + elem.NOMBRE);
+				Console.WriteLine("Obra social: " + elem.OBRA);
+				Console.WriteLine("Diagnostico: " + elem.DIAG);
+				Console.WriteLine("Dni: " + elem.DNI);
+				Console.WriteLine("Numero de afiliado: " + elem.NUMOB);
+				Console.WriteLine("---------------");
+			}
+		}
+		
+		
+	}
+}
